5c29924eb43f9e1d124c02c1d3dafa876be440f8
ArjunAranetaCodes <arjunaraneta@gmail.com>
2018-02-03 08:57:32 +0800

    2018 Updates

---
+++ Conversions/problem13.go
@@ -0,0 +1,16 @@
+//Problem 13: Write a program that converts celsius to farenheit.
+package main
+
+import "fmt"
+
+func main(){
+ celsius := 0.0
+ farenheit := 0.0
+
+ fmt.Print("Enter value of celsius: ")
+ fmt.Scan(&celsius)
+
+ farenheit = (9.0/5.0) * celsius + 32.0
+
+ fmt.Println(celsius, "C equals to ", farenheit, "F")
+}
