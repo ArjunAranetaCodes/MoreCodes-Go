ca95b4ff0c70c63e1f69b46e4d5f4f1d4ee183db
ArjunAranetaCodes <arjunaraneta@gmail.com>
2018-02-04 08:05:31 +0800

    Go Problem Update

---
+++ Conversions/problem14.go
@@ -0,0 +1,12 @@
+//Problem 14: Write a program that converts lower case to upper case characters.
+package main
+
+import (
+  "fmt"
+  "strings"
+)
+
+func main() {
+  str := "abc"
+  fmt.Println(strings.ToUpper(str))
+}
