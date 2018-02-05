81c8fb80e9c97bd2ff6114df435ea4b30f9d5536
ArjunAranetaCodes <arjunaraneta@gmail.com>
2018-02-05 21:10:36 +0800

    2018 Updates

---
+++ Conversions/problem15.go
@@ -0,0 +1,13 @@
+//Problem 15: Write a program that converts letters to ASCII.
+package main
+
+import (
+ "strconv"
+ "fmt"
+)
+
+func main() {
+ num := 10
+ strnum := strconv.Itoa(num)
+ fmt.Println(strnum)
+}
