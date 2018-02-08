2f68f94266c1324a4d04222c791b0c4cecad1b1d
ArjunAranetaCodes <arjunaraneta@gmail.com>
2018-02-09 05:40:26 +0800

    2018 Updates

---
+++ Conversions/problem17.go
@@ -0,0 +1,13 @@
+//Problem 17: Write a program that converts kilometer to meter.
+package main
+
+import (
+ "fmt"
+)
+
+func main(){
+ kilometer := 1
+ meter := kilometer  * 12
+ 
+ fmt.Print(meter)
+}
