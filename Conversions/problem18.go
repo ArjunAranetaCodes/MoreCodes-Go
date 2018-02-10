763a758610e8eb6bdaca63063f3b42bdb5573dfa
ArjunAranetaCodes <arjunaraneta@gmail.com>
2018-02-10 09:39:36 +0800

    2018 Updates

---
+++ Conversions/problem18.go
@@ -0,0 +1,13 @@
+//Problem 18: Write a program that converts liter to milliliter.
+package main
+
+import (
+ "fmt"
+)
+
+func main(){
+ liter := 1
+ milliliter := liter * 12
+ 
+ fmt.Print(milliliter)
+}
