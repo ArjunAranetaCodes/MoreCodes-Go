eb0c860c49b2bfe78a10d2c4783f368db37ff334
ArjunAranetaCodes <arjunaraneta@gmail.com>
2018-11-03 05:00:00 +0100

    2018 Updates

---
+++ Number Patterns/problem13.go
@@ -0,0 +1,33 @@
+/*
+Problem 13: Write a program to print the pattern of asterisks using loop.
+*
+**
+***
+****
+*****
+****
+***
+**
+*
+*/
+
+package main
+
+import "fmt"
+
+func main() {
+	row := 5;
+	for y := 0; y <= row; y++ {
+		for x := 0; x < y; x++ {
+			fmt.Print("*");	
+		}
+		fmt.Print("\n");	
+	}
+	
+	for y := row - 1; y > 0; y-- {
+		for x := 0; x < y; x++ {
+			fmt.Print("*");	
+		}
+		fmt.Print("\n");	
+	}
+}
